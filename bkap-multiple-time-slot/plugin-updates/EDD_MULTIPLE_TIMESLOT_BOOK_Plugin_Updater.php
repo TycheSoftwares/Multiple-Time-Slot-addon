f149a7c3c54d5901906cf14a55ff507481ba9e66
Pinal <pinalj1612@gmail.com>
2017-04-11 22:07:19 +0530

    Version 2.0
    Updated the plugin version number to 2.0

--- bkap-multiple-time-slot/plugin-updates/EDD_MULTIPLE_TIMESLOT_BOOK_Plugin_Updater.php
+++ bkap-multiple-time-slot/plugin-updates/EDD_MULTIPLE_TIMESLOT_BOOK_Plugin_Updater.php
@@ -7,7 +7,7 @@
  * Allows plugins to use their own update API.
  *
  * @author Ashok Rane
- * @version 1.9
+ * @version 2.0
  */
 class EDD_MULTIPLE_TIMESLOT_BOOK_Plugin_Updater {
 	private $api_url  = 'http://www.tychesoftwares.com/';
